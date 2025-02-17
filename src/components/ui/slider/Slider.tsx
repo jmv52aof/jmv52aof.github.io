9392b65ea154d50d8beb71c94203bbe0456ef562
Fedor <petuchov.fedya.a.eto.ya@gmail.com>
2025-02-17 17:06:12 +0300

    Переработал логику генерации маркеров, добавил стили для hover, active, disabled

--- src/components/ui/slider/Slider.tsx
+++ src/components/ui/slider/Slider.tsx
@@ -1,11 +1,11 @@
 import styles from './styles.module.scss'
 import { initMarkers } from './lib/finction';
+import { JSX, useState } from 'react';
 
 interface SliderProps {
-	min: number | string;
-	max: number | string;
-	step: number | string;
-	defaultValue: number | string;
+	valueLst: number[]
+	step: number
+	generateMarker: (value: number, index: number) => string
 	onChange: (value: string) => void;
 }
 
@@ -13,25 +13,37 @@ interface SliderProps {
  * Горизонтальный ползунок, перемещаемый мышкой вдоль прямой
  */
 export default function Slider(props: SliderProps): React.JSX.Element {
-	const markers = initMarkers(Number(props.min), Number(props.max), Number(props.step))
+	const [disabled, setDisabled] = useState<boolean>(false)
 	const handleChange = (value: string) => {
 		props.onChange(value)
-	}	
-
+	}
+	const min = props.valueLst[0]
+	const max = props.valueLst[props.valueLst.length - 1]
+	let valIdx = 0
+	const markers: JSX.Element[] = []
+	
+	for (let i = min; i <= max; i += props.step) {
+		if (props.valueLst[valIdx] === i) {
+			markers.push(<span key={i} style={{'color': disabled ? '#C8CED8' : '#373435'}}>{props.generateMarker(props.valueLst[valIdx], valIdx)}</span>)
+			valIdx++;
+		} else {
+			markers.push(<span key={i}> </span>)
+		}
+	}
 	return(
 		<div className={styles.slider}>
 			<div className={styles.slider__wrapper}>
 				<input 
+					disabled={disabled}
 					type="range"
-					min={props.min}
-					max={props.max}
+					min={props.valueLst[0]}
+					max={props.valueLst[props.valueLst.length - 1]}
 					className={styles.wrapper__range}
-					step={props.step}
-					defaultValue={props.defaultValue}
+					step={props.step}					
 					onChange={(event) => handleChange(event.target.value)}		
 				/>
 				<div className={styles.wrapper__markers}>
-					{markers.map(item => <span key={item}>{item}</span>)}
+					{markers.map(item => item)}
 				</div>
 			</div>			
 		</div>)
--- src/components/ui/slider/lib/finction.ts
+++
@@ -1,7 +0,0 @@
-export const initMarkers = (min: number, max: number, step: number) => {
-	const result = [];
-	for (let i = min; i <= max; i += step) {
-		result.push(i);
-	}
-	return result;
-}
