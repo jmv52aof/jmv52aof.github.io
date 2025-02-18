e51936070bf55713b02d7cb2febb7e32f2f67224
Fedor <petuchov.fedya.a.eto.ya@gmail.com>
2025-02-18 13:41:39 +0300

    Доделал столбцы

--- src/components/dailyOccupation/DailyOccupation.tsx
+++ src/components/dailyOccupation/DailyOccupation.tsx
@@ -19,45 +19,54 @@ const weekDayNamesMap = new Map<number, string>([
 
 
 export default function DailyOccupation({data, chartHeight, chartWidth}: Props): React.JSX.Element {
-    const MARGIN = 50
-    const width = chartWidth - MARGIN * 2
-    const height = chartHeight - MARGIN * 2
-    const xAxisY = MARGIN + height;
-    const dataYMax = data.reduce(
-        (currMax, item) => Math.max(currMax, item.occupancy_in_percentage),
-        -Infinity
-    );
-    const dataYMin = data.reduce(
-        (currMin, item) => Math.min(currMin, item.occupancy_in_percentage),
-        Infinity
-    );
-    const dataYRange = dataYMax - dataYMin;    
-    
+    const margin = 50
+    const width = chartWidth - margin * 2
+    const height = chartHeight - margin * 2  
     const barPlotWidth = width / data.length;
-    
+    const sidePadding = 10
+    const textPadding = 25
+    const color = '#0000ff'
+
+    const getColor = (percent: number)  => {
+        // Преобразуем HEX в RGB
+        let r = parseInt(color.substring(1, 3), 16);
+        let g = parseInt(color.substring(3, 5), 16);
+        let b = parseInt(color.substring(5, 7), 16);
+
+        // Интерполируем цвет между белым (255, 255, 255) и исходным
+        r = Math.round(255 - (255 - r) * (percent / 100));
+        g = Math.round(255 - (255 - g) * (percent / 100));
+        b = Math.round(255 - (255 - b) * (percent / 100));
+
+        // Преобразуем RGB обратно в HEX
+        const toHex = (c: number) => {
+            const hex = c.toString(16);
+            return hex.length === 1 ? '0' + hex : hex;
+        };
+
+        return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+    }
+
 	return (
         <div className={styles.block}>
             <h2 className={styles.block__title}>График загруженности</h2>
             <div className={styles.block__chart}>
                 <svg width={chartWidth} height={chartHeight} className={styles.chart}>
                     {data.map((item, index) => {                    
-                        const x = MARGIN + index * barPlotWidth;
-
-                        const yRatio = (item.occupancy_in_percentage - dataYMin) / dataYRange;
-
-                        const y = MARGIN + (1 - yRatio) * height;
-                        const heightBar = yRatio * height;
-
-                        const sidePadding = 10;
+                        const x = margin + index * barPlotWidth;                                            
+                        const y = height - item.occupancy_in_percentage * height / 100;
+                        const heightBar = item.occupancy_in_percentage * height / 100;
                         return (
                             <g key={index}>
                               <rect
                                 x={x + sidePadding / 2}
                                 y={y}
+                                rx={10}
+                                fill={getColor(item.occupancy_in_percentage)}
                                 width={barPlotWidth - sidePadding}
                                 height={heightBar}
                               />
-                              <text x={x + barPlotWidth / 2} y={xAxisY + 16} textAnchor="middle">
+                              <text x={x + barPlotWidth / 2} y={y + heightBar + textPadding} textAnchor="middle">
                                 {weekDayNamesMap.get(item.weekday)}
                               </text>
                             </g>
