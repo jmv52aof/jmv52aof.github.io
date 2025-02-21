f8859b2ba7fae23b8007cdf8b216ec0570e75517
Fantasy <dmitriypet03@bk.ru>
2025-02-21 10:46:17 +0300

    [Search] Добавил компонент search

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -2,7 +2,6 @@ import StationsMap from '@features/stationsMap/StationsMap'
 import styles from './styles.module.scss'
 import commonStyles from '@common/styles.module.scss'
 import ControlPanel from '@features/controlPanel/ControlPanel'
-import Search from '@components/search/Search'
 import Button from '@components/ui/button/Button'
 import Switch from '@components/ui/switch/Switch'
 import Status from '@components/ui/status/Status'
@@ -15,13 +14,12 @@ export default function MainPage(): React.JSX.Element {
 	return (
 		<div className={commonStyles.page}>
 			<div className={styles.header}>
-				<Search />
 				<Button iconSrc={tuningImage} onClick={() => {}} variant='icon' />
-				<Switch onChange={(enabled) => { }} />	
-				<Status textSize='small' color='green' text='Доступен'/>
-				<Status textSize='medium' color='orange' text='Занят'/>
-				<Status textSize='medium' color='red' text='Нет соединения'/>
-				<Status textSize='large' color='grey' text='Невалидна'/>
+				<Switch onChange={enabled => {}} />
+				<Status textSize='small' color='green' text='Доступен' />
+				<Status textSize='medium' color='orange' text='Занят' />
+				<Status textSize='medium' color='red' text='Нет соединения' />
+				<Status textSize='large' color='grey' text='Невалидна' />
 			</div>
 			<StationsMap />
 			<div className={styles.footer}>
