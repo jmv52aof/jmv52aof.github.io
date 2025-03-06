c824cba679d932ae325655a684fed0822629337d
Dmitriy <dmitriypet03@bk.ru>
2025-03-06 11:56:26 +0300

    [UPD] Увеличил текст в фильтрах станций

--- src/components/ui/connectorCard/ConnectorCard.tsx
+++ src/components/ui/connectorCard/ConnectorCard.tsx
@@ -1,42 +1,45 @@
-import { useState, useEffect } from "react";
-import styles from "./styles.module.scss";
-import { ConnectorStandard } from "@common/types/stations";
-import { CONNECTOR_HAS_ICON } from "@common/consts/stations";
+import { useState, useEffect } from 'react'
+import styles from './styles.module.scss'
+import { ConnectorStandard } from '@common/types/stations'
+import { CONNECTOR_HAS_ICON } from '@common/consts/stations'
 
 type Props = {
-    onChange: (enabled: boolean) => void;
-    enabled?: boolean;
-    connector: ConnectorStandard;
-    disabled?: boolean;
-};
+	onChange: (enabled: boolean) => void
+	enabled?: boolean
+	connector: ConnectorStandard
+	disabled?: boolean
+}
 
 export default function ConnectorCard(props: Props) {
-    const [isEnabled, setIsEnabled] = useState<boolean>(props.enabled ?? true);
+	const [isEnabled, setIsEnabled] = useState<boolean>(props.enabled ?? true)
 
-    useEffect(() => {
-        setIsEnabled(props.enabled ?? true);
-    }, [props.enabled]);
+	useEffect(() => {
+		setIsEnabled(props.enabled ?? true)
+	}, [props.enabled])
 
-    const handleClick = () => {
-        if (!props.disabled) {
-            setIsEnabled(!isEnabled);
-            props.onChange(!isEnabled);
-        }
-    };
+	const handleClick = () => {
+		if (!props.disabled) {
+			setIsEnabled(!isEnabled)
+			props.onChange(!isEnabled)
+		}
+	}
 
-    return (
-        <div 
-            className={`${styles.card} ${isEnabled ? styles.card_enabled : ""} ${props.disabled ? styles.card_disabled : ""}`} 
-            onClick={handleClick}
-        >
-            <div className={styles.card__content}>
-                <div className={styles.content__icon}>
-                    <img src={CONNECTOR_HAS_ICON[props.connector]} alt={props.connector} />
-                </div>
-                <div className={styles.content__title}>
-                    <span className={styles.title__text}>{props.connector}</span>
-                </div>
-            </div>     
-        </div>
-    );
+	return (
+		<div
+			className={`${styles.card} ${isEnabled ? styles.card_enabled : ''} ${
+				props.disabled ? styles.card_disabled : ''
+			}`}
+			onClick={handleClick}
+		>
+			<div className={styles.card__content}>
+				<div className={styles.content__icon}>
+					<img
+						src={CONNECTOR_HAS_ICON[props.connector]}
+						alt={props.connector}
+					/>
+				</div>
+				<a className={styles.content__text}>{props.connector}</a>
+			</div>
+		</div>
+	)
 }
