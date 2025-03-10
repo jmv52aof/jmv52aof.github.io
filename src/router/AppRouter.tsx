040e79d1dcada7d64d9579694cded67d6419a4c4
belovnv <blovnv@gmail.com>
2025-03-10 11:20:28 +0300

    Сверстал страницу Способ оплаты

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router'
 import * as endpoints from '@common/consts/endpoints'
 import StationsPage from '@pages/stations/Stations'
 import StationsFiltersPage from '@pages/stationsFilters/Filters'
+import PaymentMethodPage from '@pages/paymentMethod/PaymentMethod'
 
 export default function AppRouter(): React.JSX.Element {
 	return (
@@ -9,6 +10,10 @@ export default function AppRouter(): React.JSX.Element {
 			<Route
 				path={endpoints.STATIONS_FILTERS_ENDPOINT}
 				element={<StationsFiltersPage />}
+			/>
+			<Route
+				path={endpoints.PAYMENT_METHOD_ENDPOINT}
+				element={<PaymentMethodPage />}
 			/>
 			<Route
 				path={endpoints.STATIONS_LIST_ENDPOINT}
