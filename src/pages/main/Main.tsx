bb5ddd952c57d76f5c09215e4b604f67b3a9dd33
Dmitriy <dmitriypet03@bk.ru>
2025-03-12 19:40:54 +0300

    [UPD] Убрал вывод в консоль

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,10 +1,6 @@
 import StationsMap from '@features/stationsMap/StationsMap'
 import styles from './styles.module.scss'
 import ControlPanel from '@features/controlPanel/ControlPanel'
-import { useContext, useEffect, useState } from 'react'
-import { RootStateContext } from 'contexts/RootStateContext'
-import { useApi } from '@common/hooks/api'
-import { createGetStationsRequestOptions } from '@common/functions/stations'
 import FiltersButton from '@components/ui/filtersButton/FiltersButton'
 import tuningImage from '@assets/images/tuning.svg'
 import { STATIONS_FILTERS_ENDPOINT } from '@common/consts/endpoints'
@@ -16,6 +12,7 @@ import {
 import { useNavigate } from 'react-router'
 import ActiveSessionNotify from '@components/activeSessionNotify/ActiveSessionNotify'
 import Search from '@components/ui/search/Search'
+import { useStationsLoader } from './lib/hooks'
 
 /**
  * Главная страница с картой станций
@@ -23,23 +20,7 @@ import Search from '@components/ui/search/Search'
 export default function MainPage(): React.JSX.Element {
 	const nav = useNavigate()
 
-	const { setStations, stationFilters } = useContext(RootStateContext)
-	const { getStationsFromApi } = useApi()
-
-	const [loading, setLoading] = useState<boolean>(true)
-
-	useEffect(() => {
-		if (!stationFilters.shouldUpdateStations) {
-			setLoading(false)
-			return
-		}
-
-		getStationsFromApi(createGetStationsRequestOptions(stationFilters))
-			.then(res => {
-				setStations(res)
-			})
-			.finally(() => setLoading(false))
-	}, [stationFilters])
+	const { stationsLoading } = useStationsLoader()
 
 	const onFiltersClick = () => {
 		nav(
@@ -55,7 +36,7 @@ export default function MainPage(): React.JSX.Element {
 
 	return (
 		<div>
-			{!loading && (
+			{!stationsLoading && (
 				<div className={styles.header}>
 					<Search placeholder='Поиск' variant='shadow' disabled />
 					<FiltersButton
@@ -68,8 +49,8 @@ export default function MainPage(): React.JSX.Element {
 					</div>
 				</div>
 			)}
-			<StationsMap loading={loading} />
-			{!loading && (
+			<StationsMap loading={stationsLoading} />
+			{!stationsLoading && (
 				<div className={styles.footer}>
 					<ControlPanel />
 				</div>
