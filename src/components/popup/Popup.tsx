11001df9b8112016e1b0f28f9a6c2003dbecca41
belovnv <blovnv@gmail.com>
2025-03-18 20:12:04 +0300

    Добавил закрытие при клике и паддинги

--- src/components/popup/Popup.tsx
+++ src/components/popup/Popup.tsx
@@ -3,27 +3,29 @@ import { useSwipeable } from 'react-swipeable';
 import styles from './styles.module.scss';
 
 type Props = {
-	children: React.ReactNode,
-	onClose: () => void,
-    isActive: Boolean,
-}
+  children: React.ReactNode;
+  onClose: () => void;
+  isActive: boolean;
+};
 
-const Popup: React.FC<Props> = (
-	props: Readonly<Props>,
-) => {
-    const handlers = useSwipeable({
-        onSwipedDown: () => props.onClose(),
-        trackMouse: true,
-    });
+const Popup: React.FC<Props> = (props: Readonly<Props>) => {
+  const handlers = useSwipeable({
+    onSwipedDown: () => props.onClose(),
+    trackMouse: true,
+  });
 
-	return (
-        <div className={`${styles.popup} ${props.isActive ? styles.active : ''}`}>
-            <div className={styles.popup__content} {...handlers} onClick={(e) => e.stopPropagation()}>
-                <div className={styles.content__topLine}></div>
-                {props.children}
-            </div>
-        </div>
-	)
-}
+  return (
+    <div className={`${styles.popup} ${props.isActive ? styles.active : ''}`}>
+      <div
+        className={styles.popup__content}
+        {...handlers}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className={styles.content__topLine}></div>
+        {props.children}
+      </div>
+    </div>
+  );
+};
 
 export default Popup;
