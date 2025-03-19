5251cb2ed083afce36553c7e30b22c5efa4d52a6
Fantasy <dmitriypet03@bk.ru>
2025-03-19 09:37:28 +0300

    [FIX] Поправил обработку ответа (всегда json)

--- src/common/functions/requests.ts
+++ src/common/functions/requests.ts
@@ -39,19 +39,19 @@ export async function sendRequest(
 
 		let jsonResponse = undefined
 		try {
-			jsonResponse = await (options.responseIsString
-				? response.text()
-				: response.json())
+			jsonResponse = await response.json()
 		} catch (err) {}
 
+		const isError = 'error' in jsonResponse
+
 		return {
-			data: jsonResponse?.error ? undefined : jsonResponse,
-			error: jsonResponse?.error
-				? {
-						status: jsonResponse?.status,
-						error: jsonResponse?.error,
-						message: jsonResponse?.message,
-				  }
+			data: isError ? undefined : jsonResponse,
+			error: isError
+				? ({
+						status: jsonResponse.status,
+						error: jsonResponse.error,
+						message: jsonResponse.message,
+				  } as ResponseError)
 				: undefined,
 		}
 	} catch (err) {
