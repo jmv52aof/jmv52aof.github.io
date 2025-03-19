2200a3d6ff061874a7d92d4758b5aebe0afe357d
Fantasy <dmitriypet03@bk.ru>
2025-03-19 10:20:10 +0300

    [FIX] Описание ошибки и техподдержка в контенте поп-ап

--- src/components/confirmationPopupContent/ConfirmationPopupContent.tsx
+++ src/components/confirmationPopupContent/ConfirmationPopupContent.tsx
@@ -1,70 +1,78 @@
-import React, { useState } from 'react';
-import Button from '@components/ui/button/Button';
-import { Loader } from '@components/ui/loader/Loader';
-import styles from './styles.module.scss';
-import errorIcon from '@assets/images/status/error.svg';
-import { ResponseError } from '@common/types/requests';
+import React, { useState } from 'react'
+import Button from '@components/ui/button/Button'
+import { Loader } from '@components/ui/loader/Loader'
+import styles from './styles.module.scss'
+import errorIcon from '@assets/images/status/error.svg'
+import { ResponseError } from '@common/types/requests'
 
 type Props = {
-  title: string;
-  description?: string;
-  onConfirm: () => Promise<ResponseError | undefined>;
-  onClose: () => void;
-  errorTitle?: string;
-};
+	title: string
+	description?: string
+	onConfirm: () => Promise<ResponseError | undefined>
+	onClose: () => void
+	errorTitle?: string
+}
 
 export default function ConfirmationPopupContent(
-  props: Props,
+	props: Props
 ): React.JSX.Element {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<ResponseError | null>(null);
+	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState<ResponseError | null>(null)
 
-  const handleConfirm = () => {
-    setIsLoading(true);
-    props
-      .onConfirm()
-      .then((res) => {
-        if (!res) {
-          props.onClose();
-        } else {
-          setError(res);
-        }
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  };
+	const handleConfirm = () => {
+		setIsLoading(true)
+		props
+			.onConfirm()
+			.then(res => {
+				if (!res) {
+					props.onClose()
+				} else {
+					setError(res)
+				}
+			})
+			.finally(() => {
+				setIsLoading(false)
+			})
+	}
 
-  if (isLoading) {
-    return (
-      <div className={styles.content}>
-        <p className={styles.title}>Отправляем запрос...</p>
-        <Loader />
-      </div>
-    );
-  }
+	if (isLoading) {
+		return (
+			<div className={styles.content}>
+				<p className={styles.title}>Отправляем запрос...</p>
+				<Loader />
+			</div>
+		)
+	}
 
-  return (
-    <div className={styles.content}>
-      <p className={styles.title}>
-        {error
-          ? props.errorTitle || 'Произошла непредвиденная ошибка'
-          : props.title}
-      </p>
-      {props.description && (
-        <p className={styles.description}>{props.description}</p>
-      )}
-      {error ? (
-        <>
-          <img src={errorIcon} alt='Error' className={styles.icon} />
-          <Button variant='fill' onClick={props.onClose} text='Техподдержка' />
-        </>
-      ) : (
-        <div className={styles.buttons}>
-          <Button variant='text' onClick={props.onClose} text='Отменить' />
-          <Button variant='fill' onClick={handleConfirm} text='Подтвердить' />
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className={styles.content}>
+			<p className={styles.title}>
+				{error
+					? props.errorTitle || 'Произошла непредвиденная ошибка'
+					: props.title}
+			</p>
+			{(undefined !== props.description || error) && (
+				<p className={styles.description}>
+					{props.description ?? error?.message}
+				</p>
+			)}
+			{error ? (
+				<>
+					<img src={errorIcon} alt='Error' className={styles.icon} />
+					<Button
+						variant='fill'
+						onClick={() =>
+							window.location.replace(import.meta.env.VITE_TELEGRAM_SUPPORT_URL)
+						}
+						text='Техподдержка'
+					/>
+				</>
+			) : (
+				<div className={styles.buttons}>
+					<Button variant='text' onClick={props.onClose} text='Отменить' />
+					<Button variant='fill' onClick={handleConfirm} text='Подтвердить' />
+				</div>
+			)}
+		</div>
+	)
 }
