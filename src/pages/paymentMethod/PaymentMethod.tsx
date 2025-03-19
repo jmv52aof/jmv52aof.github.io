c28cc75e04228288d90a755ce6ae76a7cf1a3457
Fantasy <dmitriypet03@bk.ru>
2025-03-19 11:11:51 +0300

    [UPD] Удаление способа оплаты

--- src/pages/paymentMethod/PaymentMethod.tsx
+++ src/pages/paymentMethod/PaymentMethod.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import styles from './styles.module.scss'
 import ActionButton from '@components/ui/actionButton/ActionButton'
 import PaymentMethod from '@features/paymentMethod/PaymentMethod'
@@ -6,11 +6,18 @@ import { useNavigate } from 'react-router'
 import PageLayout from '@layouts/pageLayout/PageLayout'
 import { usePaymentMethodLoader } from './lib/hooks'
 import { RootStateContext } from '@contexts/RootStateContext'
+import PopupWrapper from '@features/popupWrapper/PopupWrapper'
+import ConfirmationPopupContent from '@components/confirmationPopupContent/ConfirmationPopupContent'
+import { useApi } from '@common/hooks/api'
 
 export default function PaymentMethodPage(): React.JSX.Element {
 	const nav = useNavigate()
+
+	const { deletePaymentMethodFromApi } = useApi()
 	const { loading } = usePaymentMethodLoader()
-	const { paymentMethod } = useContext(RootStateContext)
+	const { paymentMethod, showSnackbar } = useContext(RootStateContext)
+
+	const [popupIsOpen, setPopupIsOpen] = useState<boolean>(false)
 
 	return (
 		<PageLayout
@@ -18,6 +25,24 @@ export default function PaymentMethodPage(): React.JSX.Element {
 			title='Способ оплаты'
 			loading={loading}
 		>
+			<PopupWrapper isOpen={popupIsOpen} onClose={() => setPopupIsOpen(false)}>
+				<ConfirmationPopupContent
+					title={
+						<>
+							Вы действительно хотите <br /> удалить способ оплаты?
+						</>
+					}
+					errorTitle={
+						<>
+							Не удалось удалить <br /> способ оплаты
+						</>
+					}
+					onConfirm={() => deletePaymentMethodFromApi({})}
+					onSuccess={() => showSnackbar('warning', 'Способ оплаты удалён')}
+					onClose={() => setPopupIsOpen(false)}
+				/>
+			</PopupWrapper>
+
 			<div className={styles.page__main}>
 				<PaymentMethod paymentMethod={paymentMethod} />
 			</div>
@@ -27,9 +52,7 @@ export default function PaymentMethodPage(): React.JSX.Element {
 				}
 				variant={paymentMethod ? 'red' : 'green'}
 				onClick={() => {
-					console.log(
-						paymentMethod ? 'Удалить способ оплаты' : 'Привязать способ оплаты'
-					)
+					if (paymentMethod) setPopupIsOpen(true)
 				}}
 			/>
 		</PageLayout>
