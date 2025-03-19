db707cf4ebbf512eb6d38c2d498ff78217363ba9
In1or <71268843+In1or@users.noreply.github.com>
2025-03-19 12:34:10 +0300

    [carousel] Исправил точки и растягивание фото

--- src/components/ui/carousel/Carousel.tsx
+++ src/components/ui/carousel/Carousel.tsx
@@ -1,60 +1,50 @@
-import { useState } from "react";
 import styles from './styles.module.scss'
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
 
 type Props = {
-	imageSources: string[];
+	imageSources: string[]
 }
 
 /**
  * Карусель с фотографиями
  */
 export default function CustomCarousel(props: Props): React.JSX.Element {
+	const responsive = {
+		mobile: {
+			breakpoint: { max: 3000, min: 0 },
+			partialVisibilityGutter: 0,
+			items: 1,
+		},
+	}
 
-    const [currentIndex, setCurrentIndex] = useState(0);
+	const shouldShowDots = props.imageSources.length > 1
 
-    const prevSlide = () => {
-        setCurrentIndex((prev) => (prev > 0 ? prev - 1 : prev));
-    };
-    
-    const nextSlide = () => {
-        setCurrentIndex((prev) => (prev < props.imageSources.length - 1 ? prev + 1 : prev));
-    };
-    
-    const responsive = {
-        mobile: {
-            breakpoint: { max: 3000, min: 0 },
-            partialVisibilityGutter: 0,
-            items: 1
-        }
-    };
-  
-    return (
-        <Carousel 
-        responsive={responsive}
-        infinite={false}
-        autoPlay={false}
-        arrows={true}
-        showDots={true}
-        swipeable={true}
-        draggable={true}
-        containerClass={styles.carousel}
-        itemClass={styles.carousel__item}
-        beforeChange={(nextSlide) => {
-            window.getSelection()?.removeAllRanges();
-        }}      
-        >
-            {props.imageSources.map((src, index) => (
-            <img 
-            key={index} 
-            src={src} 
-            alt={`Slide ${index}`} 
-            className={styles.item__image} 
-            draggable={false} // 
-            onDragStart={(e) => e.preventDefault()} // 
-            />
-            ))}
-        </Carousel>
-    );
+	return (
+		<Carousel
+			responsive={responsive}
+			infinite={false}
+			autoPlay={false}
+			arrows={true}
+			showDots={shouldShowDots}
+			swipeable={true}
+			draggable={true}
+			containerClass={styles.carousel}
+			itemClass={styles.carousel__item}
+			beforeChange={_ => {
+				window.getSelection()?.removeAllRanges()
+			}}
+		>
+			{props.imageSources.map((src, index) => (
+				<img
+					key={index}
+					src={src}
+					alt={`Slide ${index}`}
+					className={styles.item__image}
+					draggable={false} //
+					onDragStart={e => e.preventDefault()} //
+				/>
+			))}
+		</Carousel>
+	)
 }
